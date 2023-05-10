ce1bbfa45ca0d893574afac23c3767c485710499
rahulmanjhu2 <88344968+rahulmanjhu2@users.noreply.github.com>
2023-05-10 22:51:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     plugins = dict(root="plugins")
     app = pyrogram.Client(
         "bot",
-        bot_token=os.environ.get("TOKEN"),
-        api_id=int(os.environ.get("APP_ID")),
-        api_hash=os.environ.get("API_HASH"),
+        bot_token=os.environ.get("TOKEN","5949667242:AAH9MW1nebhYP0TQyFf3kInlytQREhhHA34"),
+        api_id=int(os.environ.get("APP_ID", 2308011)),
+        api_hash=os.environ.get("API_HASH","d5c1012797406492f13dc9199bdcb753"),
         plugins=plugins,
     )
     app.run()
